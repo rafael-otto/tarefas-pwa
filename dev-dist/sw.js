00510af3f142b7f84b71cdea0918c81275d08ce2
rafael-otto <rafael.august.otto.ifc@gmail.com>
2026-08-03 13:52:17 -0300

    Feat: fazendo o passo 2 da unidade 4

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3j1hd6dahts"
+    "revision": "0.4b56jr0frp4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
